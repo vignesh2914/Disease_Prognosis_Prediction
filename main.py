c0ef881443e1a15bdc31983bb3466de0ad6c594a
vignesh <vignesh2914@gmail.com>
2024-10-21 11:48:19 +0530

    Data_ingestion calling the file part 2 completed

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-from diseaseprognosis import logger
+from diseaseprognosis import logger 
+from diseaseprognosis.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome folks")
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">stage{STAGE_NAME} STARTED<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">stage{STAGE_NAME} COMPLETED<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
